bd335b7b67f4bf4bbf92a322952f852f30075ae8
PeteHass94 <phassard@hotmail.co.uk>
2019-07-31 11:02:14 +0100

    trailer beginning

--- js/home.js
+++ js/home.js
@@ -70,6 +70,12 @@ function date_time()
         return day_hour;
 }
 
+function toTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 /*
 function darkColor()
 {
