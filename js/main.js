e66f66025be7e68f24cb40d9bec19a8ee93ead72
PeteHass94 <phassard@hotmail.co.uk>
2019-08-01 14:23:03 +0100

    booking and ordering done

--- js/main.js
+++ js/main.js
@@ -91,16 +91,12 @@ function autoFill() {
     document.getElementById("theFilmNames").selectedIndex = 0;
     option.text = "Select Movie needed";
     x.add(option);
-
-
   }
   else {
     document.getElementById('theFilmNames').value = sessionStorage.getItem("selectedFilmName");
     option.text = sessionStorage.getItem("selectedFilmTime");
     x.add(option);
   }
-
-
   document.getElementById('theFilmDate').selectedIndex = 1;
 
 
@@ -116,6 +112,40 @@ function calculateTotal() {
   return total;
 }
 
+function checkTheTime() {
+  var check = 0;
+  date = new Date;
+  h = date.getHours();
+  m = date.getMinutes();
+
+  var ticketTime = document.getElementById("theFilmTimes");
+  var selectedTime = ticketTime.options[ticketTime.selectedIndex].text.toString();
+  console.log(selectedTime);
+  console.log(selectedTime.substring(0, 2));
+  console.log(selectedTime.substring(3, 5));
+  console.log(h);
+  console.log(m);
+
+  if (selectedTime.length > 4) {
+    filmHour= selectedTime.substring(0, 2);
+    filmMinute = selectedTime.substring(3, 5);
+
+    if (h > filmHour)
+    {
+    check = 0;
+    }
+    else if (h == filmHour && m > filmMinute)
+    {
+    check = 0;
+    }
+    else
+    {
+    document.getElementById("timeCheck").innerHTML = "Time Valid";
+    check = 1;
+    }
+  }
+  return check;
+}
 function submitOrder() {
 if (typeof(Storage) !== "undefined") {
 
@@ -123,26 +153,52 @@ if (typeof(Storage) !== "undefined") {
   orderedDate = orderedDate.options[orderedDate.selectedIndex].text;
   sessionStorage.setItem("orderDate", orderedDate);
 
-  var orderedMovie = document.getElementById("theFilmTitle");
+  var orderedMovie = document.getElementById("theFilmNames");
   orderedMovie = orderedMovie.options[orderedMovie.selectedIndex].text;
   sessionStorage.setItem("orderMovie", orderedMovie);
 
-  var orderedTime =document.getElementById("theFilmTime");
+  var orderedTime =document.getElementById("theFilmTimes");
   orderedTime = orderedTime.options[orderedTime.selectedIndex].text;
   sessionStorage.setItem("orderTime", orderedTime);
 
   var orderedTotal = document.getElementById("theTicketOrder");
+  orderedTickets = orderedTotal.options[orderedTotal.selectedIndex].text;
+  sessionStorage.setItem("orderTickets", orderedTickets);
   orderedTotal = (orderedTotal.options[orderedTotal.selectedIndex].text)*5;
-  sessionStorage.setItem("orderDate", orderedTotal);
+  sessionStorage.setItem("orderTickets", orderedTickets);
+  sessionStorage.setItem("orderTotal", orderedTotal);
+
+  var timeCheck = checkTheTime();
+  console.log(timeCheck);
 
   if (orderedDate === "Sunday closed")
-  document.getElementById("userMessage").innerHTML = "Sorry, we are not open on sundays please change the date";
+    document.getElementById("userMessage").innerHTML = "Sorry, we are not open on sundays please change the date";
+  else if (timeCheck == 0)
+    document.getElementById("userMessage").innerHTML = "Sorry, time is invalid";
   else
-  window.location.href = "order.html";
+    window.location.href = "order.html";
 
 } else {
   document.getElementById("userMessage").innerHTML = "Sorry, your browser does not support Web Storage... make your order in person";
 }
+}
 
+function displayOrder() {
+  document.getElementById("orderDate").innerHTML = sessionStorage.getItem("orderDate");
+  document.getElementById("orderName").innerHTML = sessionStorage.getItem("orderMovie");
+  document.getElementById("orderTime").innerHTML = sessionStorage.getItem("orderTime");
+  document.getElementById("orderTickets").innerHTML = sessionStorage.getItem("orderTickets");
+  document.getElementById("orderTotal").innerHTML = "£" + sessionStorage.getItem("orderTotal");
 
+}
+
+function proceedPayment() {
+  paymentSection = document.getElementById("payments").style.display = "block";
+}
+
+//source https://stackoverfljs w.com/questions/2255291/print-the-contents-of-a-div
+function printTicket(ticket)
+{
+    paymentSection = document.getElementById("payments").style.display = "none";
+    window.print();
 }
